ad770ab44d227cebfbefd129ed96f99c5177799d
Ouwéis <ouweis.m@icloud.com>
2017-01-22 21:40:56 +0400

    Start Project4

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Ouwéis Moolna on 22/01/2017.
+//  Copyright © 2017 Ouwéis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
